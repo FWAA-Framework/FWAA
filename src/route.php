fe3f29c0f31358f9110e29bb7c310f20a9c068fd
Aaron Junker <aaron.junker@outlook.com>
2022-12-12 19:52:09 +0100

    Update EditorConfig

--- src/route.php
+++ src/route.php
@@ -1,19 +1,19 @@
 <?php
 
-namespace FWAA;
+	namespace FWAA;
 
-class route
-{
-    public $path;
-    public $controller;
-    public $action;
-    public $params;
+	class route
+	{
+		public $path;
+		public $controller;
+		public $action;
+		public $params;
 
-    public function __construct($path, $controller, $action, $params)
-    {
-        $this->path = $path;
-        $this->controller = $controller;
-        $this->action = $action;
-        $this->params = $params;
-    }
-}
+		public function __construct($path, $controller, $action, $params)
+		{
+			$this->path = $path;
+			$this->controller = $controller;
+			$this->action = $action;
+			$this->params = $params;
+		}
+	}
